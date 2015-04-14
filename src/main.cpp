d76b6fd7c712dbd8a20a21f664434d277a4c00b8
Kevin Ortega <korte004@ucr.edu>
2015-04-14 12:07:56 -0700

    added getlogin

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 using namespace std;
 
 int main()
 {
+	string username;
+	if(getlogin() == NULL)
+	{
+		perror("getline()");
+	}
+	else
+	{
+		username = getlogin();
+		cout << endl;
+	}
+
+
+
 	return 0;
 }
 
