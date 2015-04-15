0235c450880c06ff9543f4a89f20f1895b94b857
Kevin Ortega <korte004@ucr.edu>
2015-04-15 01:33:04 -0700

    Added find_connectors function

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <vector>
 #include <iostream> 
 #include <unistd.h>
 #include <stdio.h>
@@ -5,6 +6,9 @@
 #include <errno.h>
 #include <cstring>
 using namespace std;
+
+
+#define MEMORY 200000 // size of memory allocation for commands
 
 void display_user() //displays the login name followed by a '@' followed by the hostname
 {
@@ -29,29 +33,59 @@ void display_user() //displays the login name followed by a '@' followed by the 
         user = login + "@" + host + "$ "; //user is the concantinated string of login and hostname, seperated by and "@"
 	cout << user;
 }
+vector<int> find_connectors(const string command)
+{	
+	vector<int> Connectors;
+	for(unsigned int i = 0; i < command.size(); i++)
+	{
+		if((command.at(i) == '&') && (command.at(i+1) == '&'))
+		{
+			Connectors.push_back(0);
+		}
+		else if((command.at(i) == '|') && (command.at(i+1) == '|'))
+		{
+			Connectors.push_back(1);
+		}
+		else if ((command.at(i) == ';'))
+		{
+			Connectors.push_back(2);
+		}
+	}
+	return Connectors;
+}
 
-void parse(const string cmd, const string delimiter)
+vector<char*> parse(const string cmd, const string delimiter)
 {
+	vector <char*> Tokens;
 	char* delim = new char [delimiter.length()+1];
-	strcpy(delim, delimiter.c_str());
  	char* C_Array = new char [cmd.length()+1];
+	strcpy(delim, delimiter.c_str());
 	strcpy (C_Array, cmd.c_str());
+	
 	char* token = strtok (C_Array, delim);
 	while (token != 0)
 	{
-		cout << token << endl;
+		Tokens.push_back(token);
 		token = strtok(NULL,delim);
 	}
 	delete[] C_Array;
-	return;
+	delete[] delim;
+	return Tokens;
 }
 
 int main()
 {
 	string command, delimiter;
+	vector<int> my_connector;
 	display_user();
 	getline(cin, command);
-	delimiter = " ";
-	parse(command, delimiter);	
+	my_connector = find_connectors(command);
+	for(unsigned int i = 0; i < my_connector.size(); i++)
+	{
+		cout << my_connector.at(i) << endl;
+	}	
+	delimiter = "|&; ";
+	//first_parse = parse(command, delimiter);	
+	//execvp(manipulated command as a char*,arg[] with all the commands in the char array
 	
 }
