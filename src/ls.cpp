9e510019f65095691152dc75d0db45c17b36eb02
Kevin Ortega <korte004@ucr.edu>
2015-04-30 02:16:39 -0700

    fixed ls_a function

--- src/ls.cpp
+++ src/ls.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-void fill_vector(int size, vector<string> &d, vector<string> &f, char** A)
+void fill_vector(int size, char** A, vector<string> &d, vector<string> &f)
 {
 	string argument;
 	for(int i = 0; i < size ;i++)
@@ -44,13 +44,14 @@ bool found_char(vector<string> &v, char letter)
 
 }
 
-void ls-a(vector<string> &d)
+
+void ls_a(string s) 
 {
 	DIR *direct;
 	struct dirent *contents;
 	errno = 0;
-
-	if((direct = opendir(argv[1]))	== NULL)
+	const char *c = s.c_str();
+	if((direct = opendir(c)) == NULL)
 	{
 		perror("opendir()");
 		exit(1);
@@ -75,24 +76,38 @@ void ls-a(vector<string> &d)
 int main(int argc, char** argv)
 {
 	vector<string> Dirs, flags;
-	fill_vector(argc,Dirs, flags,argv);
+	fill_vector(argc,argv,Dirs, flags);
+			
 	
 		
 	//string files(filespecs ->d_name)
-	if(argc <= 1)
+	if(argc == 1)
 	{
-		// ls stuff
+		string dot = ".";
+		ls_a(dot);
 	}
 	else
 	{
 		char a = 'a'; 
 		bool found_a = found_char(flags, a);
 		vector<string> Dirs, flags;
-		fill_vector(argc,Dirs, flags,argv);
+		fill_vector(argc,argv,Dirs, flags);
 	
 		if(found_a)
 		{
-			ls-a(Dirs);
+			if(Dirs.size() == 1)
+			{
+				string dot = ".";
+				ls_a(dot);	
+			}
+			else
+			{
+				for(unsigned int i = 1; i < Dirs.size(); i++)
+				{
+					string s = Dirs.at(i);
+					ls_a(s);
+				}
+			}
 		}
 
 	}
